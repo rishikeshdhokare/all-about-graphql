ae40f0202dfe7595bd6281ff6189ff26bf46e612
Rishikesh Dhokare <rishikesh.m.dhokare@gmail.com>
2020-11-20 12:30:45 +0100

    initial commit

---
+++ src/main/kotlin/com/example/allaboutgraphql/AllAboutGraphqlApplication.kt
@@ -0,0 +1,11 @@
+package com.example.allaboutgraphql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AllAboutGraphqlApplication
+
+fun main(args: Array<String>) {
+	runApplication<AllAboutGraphqlApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/allaboutgraphql/AllAboutGraphqlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.allaboutgraphql
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AllAboutGraphqlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
