e7331f233ede875587717460461e48a30e14eeeb
Rishikesh Dhokare <rishikesh.m.dhokare@gmail.com>
2020-11-21 12:33:48 +0100

    add graphql mutation

--- src/main/kotlin/com/example/allaboutgraphql/repository/ProductRepository.kt
+++ src/main/kotlin/com/example/allaboutgraphql/repository/ProductRepository.kt
@@ -2,7 +2,13 @@ package com.example.allaboutgraphql.repository
 
 import com.example.allaboutgraphql.model.Product
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
+import java.math.BigDecimal
 
 @Repository
-interface ProductRepository: JpaRepository<Product, String>
+interface ProductRepository: JpaRepository<Product, String> {
+
+    @Query("UPDATE products SET price = ?2 WHERE id = ?1", nativeQuery = true)
+    fun updateProductPrice(productId: String, newPrice: BigDecimal)
+}
--- src/main/kotlin/com/example/allaboutgraphql/service/ProductService.kt
+++ src/main/kotlin/com/example/allaboutgraphql/service/ProductService.kt
@@ -1,12 +1,19 @@
 package com.example.allaboutgraphql.service
 
+import com.coxautodev.graphql.tools.GraphQLMutationResolver
 import com.coxautodev.graphql.tools.GraphQLQueryResolver
 import com.example.allaboutgraphql.model.Product
 import com.example.allaboutgraphql.repository.ProductRepository
 import org.springframework.stereotype.Service
+import java.math.BigDecimal
 
 @Service
-class ProductService(val productRepository: ProductRepository): GraphQLQueryResolver {
+class ProductService(val productRepository: ProductRepository): GraphQLQueryResolver, GraphQLMutationResolver {
 
     fun getAllProducts(): List<Product> = productRepository.findAll()
+
+    fun updateProductPrice(productId: String, newPrice: BigDecimal): Product {
+        productRepository.updateProductPrice(productId, newPrice)
+        return productRepository.findById(productId).get()
+    }
 }
